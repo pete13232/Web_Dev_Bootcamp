5d911e300744757760ff711790d9c62b084dff33
Pete <peetawit.vong@mail.kmutt.ac.th>
2022-11-04 11:03:20 +0700

    Split to FE, BE and big update - seconde part

---
+++ Back_End/cmd_test/index.js
@@ -0,0 +1,8 @@
+// jshint esversion6
+
+import supervillains from 'supervillains'
+// var superheroes = require('superhero-name-library');
+const supervillains = require('supervillains');
+var mySuperheroName = superheroes.random();
+var mySupervillian = supervillains.random();
+console.log(mySupervillian);
---
+++ Front_End/jQuery/index.js
@@ -0,0 +1,1 @@
+$("button").text("<em>Hey</em>");
