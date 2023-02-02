14fa0173e9184d4ff5d6e88348f8f9a05a968f33
Pete <peetawit.vong@mail.kmutt.ac.th>
2023-02-02 16:31:51 +0700

    Massive update

---
+++ fullstack-js/api/app.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var cors = require('cors')
+var app = express()
+
+app.use(cors())
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'travel'
+});
+
+connection.connect();
+
+app.get('/attractions', function (req, res, next) {
+    connection.query('SELECT * FROM attractions', function (error, results, fields) {
+        if (error) throw error;
+        res.json(results)
+    });
+
+})
+
+app.listen(3333, function () {
+    console.log('CORS-enabled web server listening on port 3333')
+})
